1613c7b170e41f85f5e47dcce9f0ac45606c62be
Aryapefo <147877759+Aryapefo@users.noreply.github.com>
2024-05-29 14:29:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openARVisualizer() {
+    alert("This would open the AR visualizer for the plant.");
+}
+
+function askGardener() {
+    const responseArea = document.getElementById('gardener-response');
+    responseArea.innerText = "This is a pre-defined response to common gardening questions.";
+}
