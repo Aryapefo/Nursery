1865120b3fce8ea246ab39a3354738d8b4ffa66c
Arya P <aryap@Aryas-MacBook-Pro-2.local>
2024-06-01 18:57:32 -0700

    Implemented AR

--- script.js
+++ script.js
@@ -25,3 +25,44 @@ function getGardenerResponse() {
 		"Sorry, I don't have an answer for that. Try another question!";
 	responseArea.innerHTML = `<p>${response}</p>`;
 }
+
+function openARVisualizer() {
+    document.getElementById('ar-visualizer').style.display = 'block';
+    dragElement(document.getElementById("draggable-plant"));
+}
+
+function closeARVisualizer() {
+    document.getElementById('ar-visualizer').style.display = 'none';
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // New position calculations will be within the modal:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
